1eb46bee01e56321e9a2ccd72b0e0512d9e11fe9
racaljk <1948638989@qq.com>
2018-04-02 18:53:02 +0800

    JavaException: fix exception printing bug

--- src/JavaException.cpp
+++ src/JavaException.cpp
@@ -11,12 +11,13 @@ void StackTrace::printStackTrace() {
     !detailedMsg.empty() ? printf("Reason:%s\n", detailedMsg.c_str()) : printf("");
 
     int deep = 0;
-    for (auto * x : exceptionStackTrace) {
+    for(size_t i=1;i<exceptionStackTrace.size();i++) {
         deep++;
-        for(int i=0;i<deep;i++) {
+        for (int i = 0; i<deep; i++) {
             printf("-");
         }
-        printf("By its caller %s()\n", x);
+
+        printf("By its caller %s()\n", exceptionStackTrace[i]);
     }
 }
 
@@ -25,6 +26,6 @@ void  StackTrace::setThrowExceptionInfo(JObject * throwableObject) {
     JObject * messageField = dynamic_cast<JObject*>(
         yrt.jheap->getObjectFieldByName(
             const_cast<JavaClass*>(throwableObject->jc), "message", "Ljava/lang/String;", throwableObject, 0)
-        );
-    exceptionStackTrace.push_back(Converter::javastring2stdtring(messageField).c_str());
+        ); 
+    detailedMsg = Converter::javastring2stdtring(messageField);
 }
