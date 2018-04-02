d20302f0ce0762be3ef40cb76fb8ba0a1f080074
racaljk <1948638989@qq.com>
2018-04-02 18:58:42 +0800

    JavaException: replace stack trace clear and sweep exception mark into an inline function

--- src/JavaException.h
+++ src/JavaException.h
@@ -9,10 +9,11 @@ public:
     void printStackTrace();
     void setThrowExceptionInfo(JObject * throwableObject);
     inline void extendExceptionStackTrace(const char * methodName) { exceptionStackTrace.push_back(methodName); }
-    inline void clearExceptionStackTrace() { exceptionStackTrace.clear(); }
+
+protected:
+    std::vector<const char *> exceptionStackTrace;
 
 private:
-    std::vector<const char *> exceptionStackTrace;
     const JavaClass * throwExceptionClass = nullptr;
     std::string detailedMsg;
 };
@@ -21,12 +22,10 @@ class JavaException : public StackTrace {
 public:
     inline bool hasUnhandledException() const { return unhandledException; }
     inline void markExpception() { unhandledException = true; }
-    inline void sweepException() { unhandledException = false; }
-
+    inline void sweepException() { unhandledException = false; exceptionStackTrace.clear(); }
 
 private:
     volatile bool unhandledException = false;
-    StackTrace exceptionStackTrace;
 };
 
 
