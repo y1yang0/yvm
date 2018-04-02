2a4fe80ecc6b9a3b856082e903906c59d901d013
racaljk <1948638989@qq.com>
2018-04-02 18:55:23 +0800

    JavaException: fix exceptio bug

--- src/JavaException.cpp
+++ src/JavaException.cpp
@@ -11,12 +11,12 @@ void StackTrace::printStackTrace() {
     !detailedMsg.empty() ? printf("Reason:%s\n", detailedMsg.c_str()) : printf("");
 
     int deep = 0;
-    for (auto * x : exceptionStackTrace) {
+    for(size_t p=1;p<exceptionStackTrace.size();p++) {
         deep++;
-        for(int i=0;i<deep;i++) {
+        for (int i = 0; i<deep; i++) {
             printf("-");
         }
-        printf("By its caller %s()\n", x);
+        printf("By its caller %s()\n", exceptionStackTrace[p]);
     }
 }
 
@@ -26,5 +26,5 @@ void  StackTrace::setThrowExceptionInfo(JObject * throwableObject) {
         yrt.jheap->getObjectFieldByName(
             const_cast<JavaClass*>(throwableObject->jc), "message", "Ljava/lang/String;", throwableObject, 0)
         );
-    exceptionStackTrace.push_back(Converter::javastring2stdtring(messageField).c_str());
+    detailedMsg = Converter::javastring2stdtring(messageField);
 }
