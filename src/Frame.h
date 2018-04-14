cf871087a39541ac693d418b7690a12b4bff1c14
racaljk <1948638989@qq.com>
2018-04-14 11:41:16 +0800

    StackFrame : give multithreading-safe guarantee by spin lcok

--- src/Frame.h
+++ src/Frame.h
@@ -2,14 +2,17 @@
 #define YVM_JAVAFRAME_H
 
 #include "JavaType.h"
-#include <stack>
 #include <deque>
+#include <mutex>
+#include <atomic>
+
+using namespace std;
 
 struct JType;
 
 struct Frame {
-    std::deque<JType*> locals;
-    std::deque<JType*> stack;
+    deque<JType*> locals;
+    deque<JType*> stack;
 
     ~Frame() {
         while (!locals.empty()){
@@ -25,6 +28,41 @@ struct Frame {
     }
 };
 
-typedef std::deque<Frame*> StackFrames;
+class SpinLock {
+public:
+    SpinLock() = default;
+    SpinLock(const SpinLock&) = delete;
+    SpinLock& operator=(const SpinLock&) = delete;
+    SpinLock(SpinLock&&) = delete;
+    SpinLock&& operator=(SpinLock&&) = delete;
 
-#endif //YVM_JAVAFRAME_H
+    inline void lock()noexcept { while (flag.test_and_set(std::memory_order_acquire)); }
+    inline void unlock()noexcept { flag.clear(std::memory_order_release); }
+private:
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+};
+
+class StackFrames {
+public:
+    StackFrames() = default;
+    ~StackFrames() {
+        lock_guard<SpinLock> lock(spinMtx);
+        while (!frames.empty()) {
+            auto temp = frames.back();
+            frames.pop_back();
+            delete temp;
+        }
+    }
+
+    inline auto back() noexcept { lock_guard<SpinLock> lock(spinMtx);return frames.back();}
+    inline void push_back(Frame * f) { lock_guard<SpinLock> lock(spinMtx); frames.push_back(f); }
+    inline void pop_back() { lock_guard<SpinLock> lock(spinMtx); frames.pop_back(); }
+    inline bool empty() { lock_guard<SpinLock> lock(spinMtx); return frames.empty(); }
+    inline auto cbegin() noexcept{ lock_guard<SpinLock> lock(spinMtx); return frames.cbegin(); }
+    inline auto cend() noexcept { lock_guard<SpinLock> lock(spinMtx); return frames.cend(); }
+private:
+    deque<Frame*> frames;
+    SpinLock spinMtx;
+};
+
+#endif
