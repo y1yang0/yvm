decf040fb7c5baf1aec63469aea4c13936fe4c51
racaljk <1948638989@qq.com>
2018-04-15 17:35:26 +0800

    README: update memory && cpu analysis graph

--- README.EN.md
+++ README.EN.md
@@ -66,10 +66,6 @@ Rewrote JDK classes are as follows:
 + Garbage Collection
 ![](./public/gc_java.png)
 ![](./public/gc_console.png)
-![](./public/gc_sampling.png)
-(Before:)
-![](./public/gc_sampling.png)
-(After improving performance:)
 ![](./public/gc_sampling_2.png)
 # License
 Code licensed under the MIT License.
--- javalib_src/ydk/test/GCTest.java
+++ javalib_src/ydk/test/GCTest.java
@@ -1,47 +1,31 @@
 package ydk.test;
-
 import ydk.lang.IO;
-
 public class GCTest {
     private String[] strings = null;
     private int cnt = 0;
     private static final int MAX = 20000;
-
     public static void produceGarbage(){
-        for(int i=0;i<10000;i++){
-            new Object();
-        }
+        for(int i=0;i<10000;i++){ new Object(); }
     }
     public static void produceArrayGarbage(){
-        for(int i=0;i<10000;i++) {
-            Object[] unused = new Object[100];
-        }
+        for(int i=0;i<10000;i++) { Object[] unused = new Object[100]; }
     }
-
     public void fullGC(){
-        for(int i=0;i<MAX;i++){
-           new Object();
-        }
+        for(int i=0;i<MAX;i++){ new Object(); }
     }
-
     public void halfGC(){
         strings = new String[MAX/2];
-        for(int i=0;i<MAX;i++){
-            if(i%2==0){
+        for(int i=0;i<MAX;i++)
+            if(i%2==0)
                 strings[cnt++] = "This is "+ i +" times to say hello to you\n";
-            }
-        }
         cnt=0;
     }
     public void print(){
-        for(int i=0;i<MAX/2;i++){
-            IO.print(strings[i]);
-        }
+        for(int i=0;i<MAX/2;i++){ IO.print(strings[i]); }
     }
-
     public static void main(String[] args){
-        //produceGarbage();
-        //produceArrayGarbage();
+        produceGarbage();
+        produceArrayGarbage();
         GCTest test = new GCTest();
         test.fullGC();
         test.fullGC();
